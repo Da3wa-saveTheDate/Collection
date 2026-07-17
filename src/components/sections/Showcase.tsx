f2d5900d8bc487bfdf1b9b6822b13b57435d9c38
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2026-07-17 23:04:35 +0300

    Remove invitation-fan 4, 5, 6

--- src/components/sections/Showcase.tsx
+++ src/components/sections/Showcase.tsx
@@ -218,27 +218,7 @@ const templates = [
     category: 'invitation fan',
     description: 'Beautiful invitation fan design.',
     image: '/invitation-fan-3/images/hero.webp'
-  },
-  {
-    id: 'invitation-fan-4',
-    title: 'Invitation Fan 4',
-    category: 'invitation fan',
-    description: 'Beautiful invitation fan design.',
-    image: '/invitation-fan-4/images/hero.webp'
-  },
-  {
-    id: 'invitation-fan-5',
-    title: 'Invitation Fan 5',
-    category: 'invitation fan',
-    description: 'Beautiful invitation fan design.',
-    image: '/invitation-fan-5/images/hero.webp'
-  },
-  {
-    id: 'invitation-fan-6',
-    title: 'Invitation Fan 6',
-    category: 'invitation fan',
-    description: 'Beautiful invitation fan design.',
-    image: '/invitation-fan-6/images/hero.webp'
+
   },
   {
     id: 'invitation-card-1',
